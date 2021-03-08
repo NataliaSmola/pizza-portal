40d520669b6522eabdaf7fcc59a4eabec4cb64e2
Natalia <smolutka15@gmail.com>
2021-03-08 16:11:31 +0100

    Init 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to dupa.
         </p>
         <a
           className="App-link"
